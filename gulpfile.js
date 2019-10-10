351e26a22a1ebd8dab58af546e9f2739f0389e98
Tamilarasan Paneerselvam <tamilarasan@ideas2it.com>
2019-10-10 12:34:22 +0530

    Fix: Remove unwanted codes   - gulp task to clean and concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
-const {src, series, dest} = require('gulp');
+const {src, series, dest, task} = require('gulp');
 const sass =  require('gulp-sass');
 sass.compiler = require('node-sass');
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
-var concat = require('gulp-concat');
-
+const concat = require('gulp-concat');
+const clean = require('gulp-clean');
 
 function transpile(cb) {
-  src('./scss/style-all.scss')
+  return src('./scss/style-all.scss')
   .pipe(sass().on('error', sass.logError))
   .pipe(rename('style-css.css'))
   .pipe(dest('./css'))
@@ -16,15 +16,15 @@ function transpile(cb) {
     suffix: ".min",
   }))
   .pipe(dest('css'));
-  cb();
 }
-function minify(cb) {
-  src('css/style-css.css')
-  .pipe(cleanCSS({compatibility: 'ie8'}))
-  .pipe(rename({
-    suffix: ".min",
-  }))
-  .pipe(dest('css'));
-  cb()
+function concatCss(cb) {
+  return src(['node_modules/bootstrap/dist/css/bootstrap.min.css', 'css/style-css.min.css'])
+  .pipe(concat('style-all.css'))
+  .pipe(dest('./css/'))
 }
-exports.default = series(transpile);
+let deleteFile = cb => {
+  return src('./css/*.css', {read: false})
+  .pipe(clean());
+}
+exports.clean = deleteFile;
+exports.default = series(deleteFile, transpile, concatCss);
